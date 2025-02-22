e43fe93b73f9adaf73eb30ba2534c495ede3ae03
BobokulovAsilbek <147586326+BobokulovAsilbek@users.noreply.github.com>
2025-02-22 17:25:37 +0500

    Update App.js
    Adding login page

--- e-learning-project/src/App.js
+++ e-learning-project/src/App.js
@@ -1,29 +1,38 @@
+// src/App.js
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from "./sections/navbar/Navbar";
 import Header from "./sections/header/Header";
 import Features from "./sections/features/Features";
 import About from "./sections/about/About";
 import Courses from "./sections/courses/Courses";
-import Team from "./sections/team/Team"
+import Team from "./sections/team/Team";
 import Testimonial from "./sections/testimonial/Testimonial";
-import './index.css'
 import Blog from "./sections/blog/Blog";
 import Footer from "./sections/footer/Footer";
-
+import Login from "./sections/login/Login";
+import './index.css';
 
 const App = () => {
-  return(
-      <main>
-          <Navbar/>
-          <Header/>
-          <Features/>
-          <About/>
-          <Courses/>
-          <Team/>
-          <Testimonial/>
-          <Blog/>
-          <Footer/>
-      </main>
-  )
-}
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={
+                    <>
+                        <Header />
+                        <Features />
+                        <About />
+                        <Courses />
+                        <Team />
+                        <Testimonial />
+                        <Blog />
+                        <Footer />
+                    </>
+                } />
+                <Route path="/login" element={<Login />} />
+            </Routes>
+        </Router>
+    );
+};
 
-export default App
+export default App;
