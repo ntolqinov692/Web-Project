4c5e2630079f7122ff9f329cd73d139590be81aa
YDavronbek <150136969+YDavronbek@users.noreply.github.com>
2025-02-24 22:10:21 +0500

    Delete tests directory

--- tests/unit/FeaturesCard.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import FeaturesCard from '../../components/FeaturesCard';
-
-test('renders FeaturesCard with icon and title', () => {
-  render(<FeaturesCard icon="test-icon.png" title="Fast Performance" />);
-  
-  expect(screen.getByText(/Fast Performance/i)).toBeInTheDocument();
-  expect(screen.getByAltText(/Fast Performance/i)).toHaveAttribute('src', 'test-icon.png');
-});
